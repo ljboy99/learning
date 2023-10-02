c215098e156b7b4219e7f23755a27b56916eee8f
Louis Didomenicis <68966648+ljboy99@users.noreply.github.com>
2023-10-02 09:36:38 -0400

    Create igpayatinlay.py

---
+++ igpayatinlay.py
@@ -0,0 +1,30 @@
+#pig latin:
+#for each word that starts with a consonant
+#move consonant to the end
+#add "ay"
+#if word starts with a vowel
+#add "way" to the end
+#dont change words like "or", "the", "on", etc
+
+vowel = "aeiouAEIOU"
+
+#print(vowel)
+englishwords = input("""Welcome to the PIG LATIN encoder
+Please provide a message to translate.
+""")
+
+englishwords1 = englishwords.split(" ")
+
+#print(englishwords1)
+
+translation = ""
+for i in englishwords1:
+    if i[0] in vowel:
+        translation += i+"way "
+    else:
+        i += i[0].lower()
+        i = i[1:]
+        #i.replace(i[0], '')
+        translation += i+"ay "
+
+print(translation)
